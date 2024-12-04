b8fc83a9ece20da5591bcef28e8bce214b4ebce5
Denise <deniselewis1120@gmail.com>
2024-12-03 16:48:55 -0800

    Protein Translation

---
+++ Python/58-protein-translation.py
@@ -0,0 +1,31 @@
+def proteins(strand):
+    protein = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP"
+    }
+
+    result = []
+    
+    for i in range(0, len(strand), 3):
+        codon = strand[i:i + 3]
+        
+        if protein[codon] == "STOP":
+            return result
+        result.append(protein[codon])
+        
+    return result
