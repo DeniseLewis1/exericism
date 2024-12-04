5dad64b87445af27bbde63832da8c877ebb53b7e
Denise <deniselewis1120@gmail.com>
2024-12-04 15:44:34 -0800

    Prime Factors

---
+++ Python/59-prime-factors.py
@@ -0,0 +1,12 @@
+def factors(value):
+    factors = []
+    divisor = 2
+
+    while value > 1:
+        while value % divisor == 0:
+            factors.append(divisor)
+            value //= divisor
+
+        divisor += 1
+    
+    return factors
