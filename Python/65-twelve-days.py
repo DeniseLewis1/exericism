1a5a075eb9af81e2814d4b04b805424a07aa5322
Denise <deniselewis1120@gmail.com>
2025-02-24 12:39:02 -0800

    Twelve days

---
+++ Python/65-twelve-days.py
@@ -0,0 +1,37 @@
+def recite(start_verse, end_verse):
+    verses = [
+        "a Partridge in a Pear Tree.",
+        "two Turtle Doves, and ",
+        "three French Hens, ",
+        "four Calling Birds, ",
+        "five Gold Rings, ",
+        "six Geese-a-Laying, ",
+        "seven Swans-a-Swimming, ",
+        "eight Maids-a-Milking, ",
+        "nine Ladies Dancing, ",
+        "ten Lords-a-Leaping, ",
+        "eleven Pipers Piping, ",
+        "twelve Drummers Drumming, "
+    ]
+
+    day = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"
+    ]
+
+    song = []
+    
+    for i in range(start_verse - 1, end_verse):
+        song.append(f"On the {day[i]} day of Christmas my true love gave to me: {''.join(verses[i::-1])}")
+
+    return song
